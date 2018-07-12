a99c44d0729e6682935e9f9c79e4d21e61bf82ab
Arielony <ariel_saher@hotmail.com>
2018-07-12 16:47:43 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# NBR nodes map
+
+Visualizes NBR nodes on the map. see the Wiki
+
+## How to run
+`npm i`
+
+Set your peerAddress to your Daemon-IP or Hostname in locations.js on Line 8!
+
+`sudo node app.js`
+Open localhost:8080.
+
+It's caching nodes' ips every 24 hrs into `.cache` folder.
+
+![Map](http://ap.imagensbrasil.org/images/2018/02/27/Screenshot_437.png)
---
+++ app.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const locations = require('./locations');
+
+locations.cacheLocations();
+setInterval(locations.cacheLocations, 180000); // amount of milliseconds between one update and another, in this case, 3 minutes
+
+app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+
+// blog home page
+app.get('/', async (req, res) => {
+    res.render('home', { locations: await locations.getCachedLocations() });
+    setInterval(locations.getCachedLocations, 12000); // 20 seconds
+});
+
+app.listen(8083);
+
+console.log('listening on port 8083');
